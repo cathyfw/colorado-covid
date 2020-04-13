026c06b687f9930bbc52bdbc4fd3efb2e618961d
Trevor Sullivan <trevortds3@gmail.com>
2020-04-13 15:25:59 -0600

    Adding route for matplotlib plot

---
+++ app/graphs.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot as plt
+
+
+def make_figure():
+    return plt.figure()
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,18 @@
-from flask import render_template
+from flask import render_template, make_response, jsonify, send_file
 from app import app
+from io import BytesIO
+from app import graphs
+
+
+@app.errorhandler(400)
+def custom400(error):
+    return make_response(jsonify({"message": error.description}), 400)
+
+
+@app.errorhandler(404)
+def custom404(error):
+    return make_response(jsonify({"message": "not found" + error.description}), 404)
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +29,13 @@ def index():
         }
     ]
     return render_template("index.html", title="Ares is a good cat", user=user, posts=posts)
+
+
+@app.route('/colorado-graph')
+def get_co_graph():
+    fig = graphs.make_figure()
+    img = BytesIO()
+    fig.savefig(img, format="png")
+    img.seek(0)
+
+    return send_file(img, mimetype="image/png")
