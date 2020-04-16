fef24015a27c235134cb512b74367f6e1953cc1c
cathyfw <cathyfieldswalsh@gmail.com>
2020-04-16 09:41:46 -0600

    read from csv, switch to plotly, include deaths and hospitalizations

--- app/graphs.py
+++ app/graphs.py
@@ -1,30 +1,61 @@
-from matplotlib import pyplot as plt
+from typing import Union
+
 import pandas as pd
+import plotly.graph_objects as go
+from pandas import DataFrame, Series
 
-def make_figure():
-    covid_cases = pd.read_pickle("/Users/catherinewalsh/covid_cases_df")
 
-    yesterday_cumulative = covid_cases["Cumulative"].iloc[-1]
-    today_cumulative = 7303
-    today_date = "2020-04-12"
-    today_df = pd.DataFrame([[today_date, today_cumulative,
-                              today_cumulative-yesterday_cumulative]], columns=covid_cases.columns)
+def today_data(today_file):
+    today_df: Union[Series, DataFrame] = pd.read_csv(today_file).dropna(axis=0, how="any")
+    today_df["value"] = today_df["value"].map(lambda x: int(x))
 
-    covid_cases = covid_cases.append(today_df, ignore_index=True)
-    covid_cases.to_pickle("covid_cases_df")
+    return today_df
 
-    x = covid_cases["Cumulative"]
-    y = covid_cases["New"]
-    avg_new = [sum(y)/len(y)]*len(y)
 
-    fig = plt.figure()
-    plt.plot(x, y)
-    plt.plot(x, avg_new)
+def make_figure(data_df):
+    covid_df = today_data("/Users/catherinewalsh/Downloads/covid19_case_summary_2020-04-15.csv")
 
-    plt.xlabel("Cumulaive Cases")
-    plt.ylabel("New Cases")
-    plt.title("New Cases vs Cumulative Cases -- log scale")
-    plt.xscale("log")
-    plt.yscale("log")
+    # State cumulative and new CASES by day -- ONSET DATE
+    cumulative_cases_by_day = covid_df.loc[covid_df["description"] == "Cumulative Number of Cases by Onset Date"]  # x
+    new_cases_by_day = covid_df.loc[covid_df["description"] == "Case Counts by Onset Date"]  # y
+
+    # State cumulative and new HSPLZ by day -- ONSET DATE
+    cumulative_hsplz_by_day = covid_df.loc[covid_df["description"] == "Cumulative Number of Hospitalizations by Onset Date"]  # x
+    new_hsplz_by_day = cumulative_hsplz_by_day["value"].diff()  # y
+
+    # State cumulative and new DEATHS by day
+    cumulative_deaths_by_day = covid_df.loc[covid_df["description"] == "Cumulative Number of Deaths by Onset Date"]  # x
+    new_deaths_by_day = cumulative_deaths_by_day["value"].diff()  # y
+
+    cases_hovertemplate = '<b>Total</b>: %{x}'+'<br><b>New</b>: %{y}</br>'+'<br><b>Date</b>: '+cumulative_cases_by_day["attribute"]
+    hsplz_hovertemplate = '<b>Total</b>: %{x}'+'<br><b>New</b>: %{y}</br>'+'<br><b>Date</b>: '+cumulative_hsplz_by_day["attribute"]
+    deaths_hovertemplate = '<b>Total</b>: %{x}'+'<br><b>New</b>: %{y}</br>'+'<br><b>Date</b>: '+cumulative_deaths_by_day["attribute"]
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=cumulative_cases_by_day["value"], y=new_cases_by_day["value"],
+                             mode='lines',
+                             name='Cases',
+                             hovertemplate=cases_hovertemplate,
+                             showlegend=True))
+
+    fig.add_trace(go.Scatter(x=cumulative_hsplz_by_day["value"], y=new_hsplz_by_day,
+                             mode='lines',
+                             name='Hospitalizations',
+                             hovertemplate=hsplz_hovertemplate,
+                             showlegend=True))
+
+    fig.add_trace(go.Scatter(x=cumulative_deaths_by_day["value"], y=new_deaths_by_day,
+                             mode='lines',
+                             name='Deaths',
+                             hovertemplate=deaths_hovertemplate,
+                             showlegend=True))
+
+    fig.update_xaxes(type="log")
+    fig.update_yaxes(type="log")
+
+    fig.update_layout(title='Cumulative vs New COVID-19 Cases, Hospitalizations, and Deaths in Colorado',
+                      xaxis_title='Cumulative Cases',
+                      yaxis_title='New Cases by Day')
 
     return fig
